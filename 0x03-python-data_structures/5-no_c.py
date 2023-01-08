cb209c9d8803013d503de15abfd41fb817377d81
obash-cpu <absalomobala@gmail.com>
2023-01-08 03:37:54 -0800

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    index = my_string.index("c", "C")
-    my_string = my_string[:index] + my_string[index+1:]
-    return my_string
+    my_string1 = list(my_string)
+    index = my_string1.index("c" or "C")
+    my_string1 = my_string1[:index] + my_string1[index+1:]
+    return my_string1
