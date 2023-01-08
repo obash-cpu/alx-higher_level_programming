a031c23a823f95f940f169aca2ba8a7f977b4487
obash-cpu <absalomobala@gmail.com>
2023-01-08 05:35:22 -0800

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        row_strings = []
-        for i in row:
-            row_strings.append("{:d}".format(i))
-            print("\t".join(row_strings))
+    for rows in matrix:
+        for column in rows:
+            print(rows, end=" ")
+        print()
