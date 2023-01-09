6b3ed6f05e28096063a8cea417f2703f4efd6c5b
obash-cpu <absalomobala@gmail.com>
2023-01-09 06:56:28 -0800

    task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
+        return (my_list)
     else:
-        newcopy = my_list[:]
-        newcopy[idx] = element
-        return newcopy
+        new_list = my_list[:]
+        new_list[idx] = element
+        return (new_list)
