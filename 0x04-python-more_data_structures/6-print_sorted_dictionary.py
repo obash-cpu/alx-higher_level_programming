fbeea65597b05951cec0f8e34003061e11b81e12
obash-cpu <absalomobala@gmail.com>
2023-01-11 04:36:32 -0800

    task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict(sorted(a_dictionary.items(), key=lambda item: item[1]))
+    newsorted = sorted(a_dictionary)
+    print(newsorted)
