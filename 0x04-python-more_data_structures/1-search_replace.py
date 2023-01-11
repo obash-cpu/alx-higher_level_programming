40e28afb7a4c55e1bcfdb4795c83f3881c482e69
obash-cpu <absalomobala@gmail.com>
2023-01-11 02:01:33 -0800

    task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-     return new_list
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
+            return my_list
