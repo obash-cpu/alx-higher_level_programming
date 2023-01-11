d3c568dde1485b985dac43f3aa883f128b2d71a5
obash-cpu <absalomobala@gmail.com>
2023-01-11 04:39:56 -0800

    task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    newsorted = sorted(a_dictionary)
-    print(newsorted)
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
