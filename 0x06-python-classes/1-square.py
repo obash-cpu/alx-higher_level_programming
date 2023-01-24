75cf3834815335280526e3507894105bb45b3e66
obash-cpu <absalomobala@gmail.com>
2023-01-24 11:09:01 -0800

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, __size):
+    def __init__(self, size):
         self.__size = size
+    def get_self(self):
+        return self.__size
+
+    def __set_size(self, value):
+        self.__size = value
