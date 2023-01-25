489d72792230e93f9a26d3025bf3565517d3eabe
obash-cpu <absalomobala@gmail.com>
2023-01-25 06:52:47 -0800

    task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
+
+class Square:
+    """class defined for square generation
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        self.area = size**2
