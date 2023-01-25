c84d71517b66a21f7b3b7b0daa0e2e58ef1bda9c
obash-cpu <absalomobala@gmail.com>
2023-01-25 06:55:59 -0800

    task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,4 +15,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        self.area = size**2
+        area = self.__size**2
+        return area
