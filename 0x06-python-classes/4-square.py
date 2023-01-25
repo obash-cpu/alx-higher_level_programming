07a29f69d6fe3194e2a547f8a9d55b15fb220fc8
obash-cpu <absalomobala@gmail.com>
2023-01-25 09:04:06 -0800

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,15 +17,15 @@ class Square:
         @size.setter
         def size(self, value):
             """Args:
-            value (int): length of one side of square
+                value (int): length of one side of square
 
             Attributes:
-            __size (int): length of one side of square
+                 __size (int): length of one side of square
 
 
              Raises:
-             TypeError: if value is not an integer
-             ValueError: if value is less than 0
+                TypeError: if value is not an integer
+                ValueError: if value is less than 0
              """
              if type(value) is not int:
                  raise TypeError('size must be an integer')
@@ -33,5 +33,13 @@ class Square:
                  raise ValueError('size must be >= 0')
              self.__size = value
         def area(self):
+            """Calulates area of square.
+
+            Attributes:
+                 __size (int): length of one side of square
+
+            Returns:
+                area (int): length of one side, squar
+            """
             area = self.__size * self.__size
             return area
