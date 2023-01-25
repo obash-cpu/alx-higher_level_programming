ff1050b8d7433d44a1c8ef882db82b78ccee080c
obash-cpu <absalomobala@gmail.com>
2023-01-25 05:56:06 -0800

    task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ defining an empty class of square
-it contains no attribues """
+it contains no attribues 
+"""
+
 class Square:
     """given the class is empty,
-    it should be passed"""
+    it should be passed
+    """
     pass
