66acdf219369bcb008c8a25ccaa17fc7ecbb3e4c
obash-cpu <absalomobala@gmail.com>
2023-02-04 22:48:46 -0800

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" 0-add_integer Module """
+
 def add_integer(a, b=98):
+    """
+    Adds two integers
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        addition of two integers
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
