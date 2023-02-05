4c065645c830b118b4a37f51612c3f1c197badc7
obash-cpu <absalomobala@gmail.com>
2023-02-04 22:18:39 -0800

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-""" define the add function to add the integers """
-
 def add_integer(a, b=98):
-    """Function that adds two integers.
-
-    Args:
-        a ((int, (float)): first arg to add to sum.
-        b ((int, (float)): second arg to add to sum. Defaults to 98.
-
-    Returns: sum of both values.
-    """
-
-    if type(a) is float:
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
         a = int(a)
-    elif type(a) is not int:
-        raise TypeError('a must be an integer')
-
-    if type(b) is float:
+    if isinstance(b, float):
         b = int(b)
-    elif type(b) is not int:
-        raise TypeError('b must be an integer')
-
     return (a + b)
