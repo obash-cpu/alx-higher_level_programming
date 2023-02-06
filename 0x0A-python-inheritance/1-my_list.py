fc05545de8f76ff9441db5d8ac042232ae4ffea3
obash-cpu <absalomobala@gmail.com>
2023-02-06 05:13:46 -0800

    task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class mylist that inherits from list"""
+def Mylist(list):
+
+    """Initializing the class"""
+    def print_sorted(self):
+
+        """Prints the soretd list"""
+        print(sorted(self))
