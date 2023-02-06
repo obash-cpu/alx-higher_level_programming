3002c400b4099ba7915e98d74b10b7abbc3fcdef
obash-cpu <absalomobala@gmail.com>
2023-02-06 02:45:56 -0800

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,4 +3,9 @@
      function that returns the list of available attributes and methods of an object
 """
 def lookup(obj):
+    """Returns attributes amd methods of an object
+    Args:
+        obj: The object
+        Return: The attributes and the methods of an object
+    """
     return dir(obj)
