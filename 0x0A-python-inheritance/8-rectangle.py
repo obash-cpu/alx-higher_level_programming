6f1c9bdbf42a6aa1495039b6b60f8bff6cb3c6b3
obash-cpu <absalomobala@gmail.com>
2023-02-11 02:00:42 -0800

    task correction

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Rectangle class Module"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
-
-
     """Rectangle class"""
     def __init__(self, width, height):
         """Initilize rectangle method"""
