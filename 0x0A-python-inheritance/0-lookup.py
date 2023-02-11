69d0dc6e2f538ffd23d53e5b8c556d65bf177dce
obash-cpu <absalomobala@gmail.com>
2023-02-11 01:13:02 -0800

    task correction

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-     function that returns the list of available attributes and methods of an object
+     function that returns the list of available attributes and methods
 """
-def lookup(obj):
+  def lookup(obj):
     """Returns attributes amd methods of an object
     Args:
         obj: The object
