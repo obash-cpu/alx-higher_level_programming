28631a265691329cf992140c34fb130a91511a72
obash-cpu <absalomobala@gmail.com>
2023-02-11 02:03:45 -0800

    task correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 """Rectangle class Module"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
@@ -14,6 +15,7 @@ class Rectangle(BaseGeometry):
     def area(self):
         """Method that returns area of rectangle"""
         return self.__width * self.__height
+
     def __str__(self):
         """Returns a string"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
