e16092cdc24c2ab53e0a68995660a1364b6a9e73
obash-cpu <absalomobala@gmail.com>
2023-02-11 04:37:35 -0800

    task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,5 @@ def say_my_name(first_name, last_name=""):
     """prnts the first name and last name and raises a type error if the name is not  astr"""
     if type(first_name) != str && type(last_name) != str:
         raise TypeError ("first_name must be a string or last_name must be a string")
-    self.first_name = first_name
-    self.last_name = last_name
+
     print("My name is {:s} {:s}".format(first_name, last_name))
