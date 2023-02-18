a29db1afa660a28e948cc223349957c851fcc37b
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:46:34 -0800

    task

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,4 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list_div.append(total)
-    return 
+    return new_list_div
