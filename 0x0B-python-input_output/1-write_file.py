75255dee26a0828ee6e55ca565c04ebf7aa29eab
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:54:44 -0800

    task

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Defines a file-writing function."""
+""" Module containing a function that writes to a file """
+
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
     """
+    Function that writes to a text file.
+    Args:
+        filename: filename
+        text: text to write
+    Raises:
+        Exception: when the file can be opened
+    """
+
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
