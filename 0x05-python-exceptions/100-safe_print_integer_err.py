e0647b18f65858603913a91b177002d217ed134b
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:38:05 -0800

    task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except:
-        sys.stderr.write("Exception: {}\n".format(e))
-        return (False)
-    return (True)
+        return True
+    except Exception as err:
+        sys.stderr.write("Exception: " + str(err) + "\n")
+        return False
