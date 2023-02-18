d2d99c4f34b2111312992dff824366cc2daafb80
obash-cpu <absalomobala@gmail.com>
2023-02-18 09:20:52 -0800

    task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""
-file: 11-student.py
-Classes:
-    -> Student
-    """
+""" Module that defines the class Student """
+
 
 class Student:
-    ''' Student class '''
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        ''' Constructor method '''
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
-        self.age = 
+        self.age = age
 
     def to_json(self, attrs=None):
-        ''' Method that returns directory description with filter '''
-        res = {}
-        if attrs:
-            for attr in attrs:
-                if attr in self.__dict__:
-                    res[attr] = self.__dict__[attr]
-                else:
-                    for attr in self.__dict__:
-                        res[attr] = self.__dict__[attr]
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
 
-            return res
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
+
     def reload_from_json(self, json):
-        ''' Replaces all attributes of the Student instance '''
-        for attr in json:
-            self.__dict__[attr] = json[attr]
+        """ Replaces all attributes of the Student instance """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
