df40871036798b87dafbfe1757daa4f123e98506
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:27:08 -0800

    task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    prints a list of anything, but only prints the integers
-    Returns the amount of integers printed
-    """
-    i = 0
-    printed = 0
-    for i in range(0, x):
+    cont = 0
+    for index in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            printed += 1
+            print('{:d}'.format(my_list[index]), end='')
+            cont = cont + 1
         except (ValueError, TypeError):
-            continue
-        print()
-        return printed
+            pass
+    print()
+    return cont
