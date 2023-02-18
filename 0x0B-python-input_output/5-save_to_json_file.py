5d50d36b052938036028a5a18cae76e8e540cb6f
obash-cpu <absalomobala@gmail.com>
2023-02-18 09:04:48 -0800

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-'''
-file: 7-save_to_json_file.py
-functions:
-    -> save_to_json_file
-    '''
-    import json
+""" Write an object to a text file using a JSON representation """
+import json
 
 
-    def save_to_json_file(my_obj, filename):
-        ''' writes Object to a text file, using JSON representation '''
-        with open(filename, 'w', encoding="utf-8") as file:
-            json.dump(my_obj, file)
+def save_to_json_file(my_obj, filename):
+    """
+    Write an object to a text file by a JSON representation
+    Args:
+        my_obj: object
+        filename: textfile name
+    Raises:
+        Exception: when the object can't be encoded
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
