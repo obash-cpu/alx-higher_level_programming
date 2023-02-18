bb607e7ecddc782c0735d530c4537a4252740644
obash-cpu <absalomobala@gmail.com>
2023-02-18 09:17:39 -0800

    task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""
-file: 11-student.py
-Classes:
-    -> Student
-"""
+""" Module that defines the class Student """
+
 
 class Student:
-    ''' Student class '''
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        ''' Constructor method '''
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        ''' Method that returns directory description with filter '''
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
 
-        if isinstance(attrs, list) and all(isinstance(attr, str)
-                for attr in attrs):
-            res = {}
-            for i in attrs:
-                if i in self.__dict__:
-                    res[i] = self.__dict__[i]
-                    return res
-                return self.__dict__
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
