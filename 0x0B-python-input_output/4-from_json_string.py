49852b50f92caf477ae1e7f24a3abfefbc29c389
obash-cpu <absalomobala@gmail.com>
2023-02-18 09:02:30 -0800

    task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-# 6-from_json_string.py
-"""Defines a JSON-to-object function."""
+""" Return an object by a JSON representation """
 import json
 
 
 def from_json_string(my_str):
-    """Return the Python object representation of a JSON string."""
+    """
+    Return an object by a JSON representation.
+    Args:
+        my_str: JSON representation
+    Raises:
+        Exception: when the string can't be decoded
+    """
+
     return json.loads(my_str)
