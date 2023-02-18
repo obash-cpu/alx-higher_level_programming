8f7a4020990b6ebcd38c030866627fc5b88801d3
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:09:20 -0800

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             count = count + 1
-        except IndexError:
+        except (IndexError):
             continue
     print("")
     return count
