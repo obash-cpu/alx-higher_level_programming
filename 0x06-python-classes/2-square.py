253ccb9b5480f23a2f7ff002dd5556e987cd2b66
obash-cpu <absalomobala@gmail.com>
2023-02-18 13:11:26 -0800

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+""" Class Square that defines a square by
+    Private instance attribute: size
+    Instantiation with optional size
+        size must be an integer
 """
-Square generation module for Python project 0x06
-"""
+
 
 class Square:
-    """
-    class defined for square generation
-    """
+    """Class constructor"""
     def __init__(self, size=0):
-
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
+        if type(size) != int:
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = size
