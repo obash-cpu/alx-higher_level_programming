f9e6e93b0226d62cc32adce6b58a96e83a7b6f2d
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:35:14 -0800

    task

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        a = 4
-        b = "name"
-        print(a / b)
-    except TypeError:
-        print("this is a type error")
+    raise TypeError("Exception raised")
