1666c0d5ae58b0eb8823384aed4dd3c62c436b01
obash-cpu <absalomobala@gmail.com>
2023-02-18 08:29:51 -0800

    task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    total = 0
     try:
-        result = a / b
-        print("{}".format(result))
-    except:
-        result = None
+        total = a / b
+        return total
+    except ZeroDivisionError:
+        total = None
+        return total
     finally:
-        print("{}".format(result))
-        return result
+        print("Inside result: {}".format(total))
