6e3774503cad4ea090e7c545792b81305294916a
obash-cpu <absalomobala@gmail.com>
2023-03-15 07:17:04 -0700

    task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- displays the number of values in a table
-INSERT INTO `first_table` (`id`, `name`) VALUES (98, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
