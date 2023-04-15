503f911c0704cbef617f13ed27443e2432ee4711
obash-cpu <absalomobala@gmail.com>
2023-04-14 23:27:05 -0700

    task correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" ensure the file is executed"""
-if __name__ == '__main__':
-""" importing modules to be used """
 
-import MySQLdb
-import sys
+    """ ensure the file is executed"""
+        if __name__ == '__main__':
+     """ importing modules to be used """
 
-""" conect to the the database """
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host='localhost', port=3306,
+    """ conect to the the database """
+
+    db = MySQLdb.connect(host='localhost', port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-""" use cursor function to querry the database """
-mycursor = db.cursor()
-mycursor.conect("SELECT * FROM  states ORDERED BY states.id ASC;")
-rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    """ use cursor function to querry the database """
+    mycursor = db.cursor()
+    mycursor.conect("SELECT * FROM  states ORDERED BY states.id ASC;")
+        rows = cur.fetchall()
+            for row in rows:
+            print(row)
