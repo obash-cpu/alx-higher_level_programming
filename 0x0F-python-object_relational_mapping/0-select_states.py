baa991a9a04c429832a433cbbfd542f3b06537c7
obash-cpu <absalomobala@gmail.com>
2023-04-15 00:17:21 -0700

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,13 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    mycur = connection.cursor()
+    mycur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = mycur.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+    mycur.close()
+    connection.close()
 Footer
