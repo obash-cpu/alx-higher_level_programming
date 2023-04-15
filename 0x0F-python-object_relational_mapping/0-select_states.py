8090b817d9ad41b5c9594d735511129efa5cacf5
obash-cpu <absalomobala@gmail.com>
2023-04-14 23:42:37 -0700

    task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3]
+                         )
     mycur = db.cursor()
     mycur.execute("SELECT * FROM states ORDER BY state.id")
     rows = cur.fetchall()
