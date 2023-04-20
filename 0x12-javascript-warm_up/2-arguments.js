f932db6a7ac125c510ce8bd0405587082a24c5cc
obash-cpu <absalomobala@gmail.com>
2023-04-20 06:40:33 -0700

    task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+
+// Script that prints "JavaScript is amazing".
+
 if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+}
+if (process.argv.length === 3) {
   console.log('Argument found');
-} else {
+}
+if (process.argv.length > 3) {
   console.log('Arguments found');
 }
