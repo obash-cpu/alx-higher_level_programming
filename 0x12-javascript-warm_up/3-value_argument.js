dbab664b7c17b08d6369fe4ff8248250d2e39b74
obash-cpu <absalomobala@gmail.com>
2023-04-20 06:43:15 -0700

    task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined)
-{
-	console.log('No argument');
-} else {
+
+// Script that prints "JavaScript is amazing".
+
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
