3ba84bf85caf2f776426de9eebcfc9a387827018
obash-cpu <absalomobala@gmail.com>
2023-04-28 01:26:07 -0700

    task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Write a Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request as Request
+
+if __name__ == "__main__":
+    with Request.urlopen('https://alx-intranet.hbtn.io/status') as response
+    content = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
