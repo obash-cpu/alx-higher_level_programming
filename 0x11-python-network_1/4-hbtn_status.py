8f63877fbc6adddda4b5b8d76c14f2543e49273c
obash-cpu <absalomobala@gmail.com>
2023-04-29 06:49:09 -0700

    task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status"""
-import requests
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
+
+
 if __name__ == "__main__":
-    page = requests.get('https://intranet.hbtn.io/status')
-    page_content = page.text
-    print("Body response:")
-    print("\t- type: {}".format(type(page_content)))
-    print("\t- content: {}".format(page_content))
+    from requests import get
+
+    html = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
