56eeb34dbb2e541c46d81afd12d9570a21640e10
obash-cpu <absalomobala@gmail.com>
2023-04-29 07:00:12 -0700

    task

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
-able File  3 lines (3 sloc)  154 Bytes
- 
-
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
+# takes in a URL and displays all HTTP methods the server will accept.
+curl -Is "$1" | grep "Allow: " | cut -d ' ' -f 2-
