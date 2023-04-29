7b49a201eeb0e778ae8549b704ee159d2d23ac32
obash-cpu <absalomobala@gmail.com>
2023-04-29 07:06:07 -0700

    task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI -w '%{response_code}' "$1" -o /dev/null
+# Status code
+curl -s -o /dev/null -I -w "%{http_code}" "$1"
