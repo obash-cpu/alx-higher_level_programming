bd390354967234818a1cf49e78c6db92dcde9035
obash-cpu <absalomobala@gmail.com>
2023-05-23 12:54:02 +0000

    scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-//reading file using fs.readfile
-
-function readFile(filePath) {
-  fs.readFile(filePath, 'utf-8', (err, content) => {
-    if (err) {
-      console.error(`Error occurred while reading the file: ${err}`);
-    } else {
-      console.log(content);
-    }
-  });
-}
-
-//checking for the file path
-if (process.argv.length < 3) {
-  console.error('Please provide the file path as an argument.');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-readFile(filePath);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
